b34dca6a6fb0db4c16b724a3eca0eef227c22de9
adrians5j <adrian@webiny.com>
2024-10-31 09:54:28 +0100

    wip

--- packages/admin-ui/src/utils.ts
+++ packages/admin-ui/src/utils.ts
@@ -2,6 +2,7 @@ import { clsx, type ClassValue } from "clsx";
 import { generateId as baseGenerateId } from "@webiny/utils/generateId";
 import { extendTailwindMerge } from "tailwind-merge";
 import { cva, type VariantProps } from "class-variance-authority";
+export { makeDecoratable } from "@webiny/react-composition";
 
 const twMerge = extendTailwindMerge({
     override: {

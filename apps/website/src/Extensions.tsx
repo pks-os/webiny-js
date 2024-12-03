1175d16047d7cb8cb8f07b09ff84ee616c05460f
adrians5j <adrian@webiny.com>
2024-12-03 06:36:38 +0100

    wip

--- apps/website/src/Extensions.tsx
+++ apps/website/src/Extensions.tsx
@@ -1,11 +1,6 @@
 // This file is automatically updated via scaffolding utilities.
 import React from "react";
-import { Extension as SpaceXElementExtension } from "space-x-element/src/website";
 
 export const Extensions = () => {
-    return (
-        <>
-            <SpaceXElementExtension />
-        </>
-    );
+    return <></>;
 };

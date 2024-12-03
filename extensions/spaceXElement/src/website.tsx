b7a51457abe556b6b1bac3e06b8e68797fe00991
adrians5j <adrian@webiny.com>
2024-12-03 06:34:50 +0100

    wip

--- extensions/spaceXElement/src/website.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { PbRenderElementPlugin } from "@webiny/app-website";
-import { SpaceX } from "./SpaceX";
-
-export const Extension = () => <PbRenderElementPlugin elementType={"spaceX"} render={SpaceX} />;
